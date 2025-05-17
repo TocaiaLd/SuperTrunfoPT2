74e9e1203886e25d5ee953c2a55254c54e10c31d
TocaiaLd <wesleysilvaalves504@gmail.com>
2025-05-17 17:50:00 -0300

    nivel iniciante

--- SuperTrunfoPT2.c
+++ SuperTrunfoPT2.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
 int main(){
     
+    //Caracteres
+    setlocale(LC_ALL, "");
+
+
     //variaveis das cartas
     char estado1, estado2;
     char codigoDaCarta1[4], codigoDaCarta2[4];
@@ -27,7 +33,7 @@ int main(){
     scanf("%s", &nomeDaCidade1);
 
     printf("Qual o tamanho população? (Deve ser numero inteiro) ");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("Qual a área? (Use o ponto no lugar da vírgula) ");
     scanf("%f", &area1);
@@ -51,7 +57,7 @@ int main(){
     scanf("%s", &nomeDaCidade2);
 
     printf("Qual o tamanho população? (Deve ser numero inteiro) ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Qual a área? (Use o ponto no lugar da vírgula) ");
     scanf("%f", &area2);
@@ -81,14 +87,21 @@ int main(){
     printf("\n\n|----Segunda Carta----|\nEstado: %c\nCodigo: %s\nCidade: %s\nPopulação:%lu\nArea: %.2f\nPib: %.2f\nNumero de ponto turisticos: %d\nDensidade populacional: %.2f hab/km²\nPib per capita: %.2f", estado2, codigoDaCarta2, nomeDaCidade2, populacao2, area2, pib2, pontosTuristicos2, densidadePopulacional2, pibPerCapita2);
 
     //monstrando a comparação entre as cartas
-    printf("\n\n|----Comparação entre cartas----|");
-    printf("\nPopulação: Carta %d venceu", populacao1>populacao2 ? 1 : 2);
-    printf("\nÁrea: Carta %d venceu",  area1>area2 ? 1 : 2);
-    printf("\nPIB: Carta %d venceu", pib1>pib2 ? 1 : 2);
-    printf("\nPontos turísticos: Carta %d venceu", pontosTuristicos1>pontosTuristicos2 ? 1 : 2);
-    printf("\nD. Populacional: Carta %d venceu", densidadePopulacional1<densidadePopulacional2 ? 1 : 2);
-    printf("\nPIB per capita: Carta %d venceu", pibPerCapita1>pibPerCapita2 ? 1 : 2);
-    printf("\nSuper Poder: Carta %d venceu", superPoder1>superPoder2 ? 1 : 2);
+    printf("\n\n|----Comparação entre cartas por população----|");
+    printf("\nCarta 1 - %s: %lu", nomeDaCidade1, populacao1);
+    printf("\nCarta 2 - %s: %lu", nomeDaCidade2, populacao2);
+
+    if(populacao1>populacao2){
+        printf("\nResultado: Carta 1(%s) venceu", nomeDaCidade1);
+
+    }else if(populacao1<populacao2){
+        printf("\nResultado: Carta 2(%s) venceu", nomeDaCidade2);
+
+    }else{
+        printf("\nResultado: Empate");
+
+    }
+    
 
     return 0;
 }
